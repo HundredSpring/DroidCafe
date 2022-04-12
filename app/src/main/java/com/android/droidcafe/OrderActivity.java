97e20f11512b4584c2c68e76df3177ad796fc994
HundredSpring <xuanbachvt1@gmail.com>
2022-04-12 11:35:53 +0700

    Nop bai

---
+++ app/src/main/java/com/android/droidcafe/OrderActivity.java
@@ -0,0 +1,18 @@
+package com.android.droidcafe;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * This activity is blank for now. Subsequent versions of the app
+ * provide input controls and a delivery method for an order.
+ */
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+    }
+}
